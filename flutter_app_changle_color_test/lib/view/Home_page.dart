c82dc64a157b9e0fa163171c2ee6c07cf20a0e41
quanganhtranvu98 <56706097+quanganhtranvu98@users.noreply.github.com>
2020-09-05 21:23:58 +0700

    update

--- flutter_app_changle_color_test/lib/view/Home_page.dart
+++ flutter_app_changle_color_test/lib/view/Home_page.dart
@@ -6,6 +6,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+   Color color=Colors.lime;
+
+   List<int> count=[0,0,0,0,0];
+   int text=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,32 +29,57 @@ class _HomePageState extends State<HomePage> {
                   CustomButtom(
                     height: double.infinity,
                     color: Colors.lime,
-                    text: '0',
-                    onTap: (){},
+                    text: count[0].toString(),
+                    onTap: (){
+                      setState(() {
+                        color=Colors.lime;
+                        text=count[0];
+                      });
+                    },
                   ),
                   SizedBox(width: 15,),
                   CustomButtom(
                     color: Colors.deepPurple,
-                    text: '0',
-                    onTap: (){},
+                    text: count[1].toString(),
+                    onTap: (){
+                      setState(() {
+                        color=Colors.deepPurple;
+                        text=count[1];
+                      });
+                    },
                   ),
                   SizedBox(width: 15,),
                   CustomButtom(
                     color: Colors.tealAccent,
-                    text: '0',
-                    onTap: (){},
+                    text: count[2].toString(),
+                    onTap: (){
+                      setState(() {
+                        color=Colors.tealAccent;
+                        text=count[2];
+                      });
+                    },
                   ),
                   SizedBox(width: 15,),
                   CustomButtom(
                     color: Colors.blue,
-                    text: '0',
-                    onTap: (){},
+                    text: count[3].toString(),
+                    onTap: (){
+                      setState(() {
+                        color=Colors.blue;
+                        text=count[3];
+                      });
+                    },
                   ),
                   SizedBox(width: 15,),
                   CustomButtom(
                     color: Colors.greenAccent,
-                    text: '0',
-                    onTap: (){},
+                    text: count[4].toString(),
+                    onTap: (){
+                      setState(() {
+                        color=Colors.greenAccent;
+                        text=count[4];
+                      });
+                    },
                   ),
 
                 ],
@@ -61,9 +90,39 @@ class _HomePageState extends State<HomePage> {
             Center(
               child: CustomButtom(
                 height: 150,
-                color: Colors.greenAccent,
-                text: '0',
-                onTap: (){},
+                color: color,
+                text: text.toString(),
+                onTap: (){
+                  setState(() {
+                    if(color==Colors.lime)
+                    {
+                      count[0]++;
+                      text=count[0];
+                    }
+                    else if(color==Colors.deepPurple)
+                    {
+                      count[1]++;
+                      text=count[1];
+                    }
+                    else if(color==Colors.tealAccent)
+                    {
+                      count[2]++;
+                      text=count[2];
+                    }
+                    else if(color==Colors.blue)
+                    {
+                      count[3]++;
+                      text=count[3];
+                    }
+                    else if(color==Colors.greenAccent)
+                    {
+                      count[4]++;
+                      text=count[4];
+                    }
+                  });
+
+
+                },
               ),
             )
           ],
